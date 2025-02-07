ade45aff7a55e8493a55cbea86e876ab8f1a1378
Karanvir Brar <brarkaran938@gmail.com>
2025-02-06 23:19:21 -0600

    Changes to the car model

--- models/car.go
+++ models/car.go
@@ -7,4 +7,9 @@ type Car struct {
 	Year int `json:"year" dynamodbav:"Year"`
 	VIN string `json:"vin" dynamodbav:"VIN"`
 	Status string `json:"status" dynamodbav:"Status"`
+	DailyRate float64 `json:"status" dynamodbav:"DailyRate`
+	Features []string `json:"features" dynamodbav:"Features"`
+	Mileage int `json:"mileage" dynamodbav:"Mileage"`
+	Transmission string   `json:"transmission" dynamodbav:"Transmission"`
+	ImageURL string   `json:"image_url" dynamodbav:"ImageURL"`
 }
--- models/renter.go
+++ models/renter.go
@@ -4,5 +4,4 @@ type Renter struct {
 	ID string `json:"id"`
 	Name string `json:"name"`
 	Email string `json:"email"`
-	CarID string `json:"car_id"`
 }
